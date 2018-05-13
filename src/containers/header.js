2ff950b466d0aa0acc7e4594e8577b47fa8c77c4
Astanios <r2d21993@gmail.com>
2018-05-13 13:48:10 -0430

    improvements

--- src/containers/header.js
+++ src/containers/header.js
@@ -6,40 +6,63 @@ import { Link } from "react-router-dom";
 import '../App.css';
 import SearchBar from '../components/searchBar';
 import logoHeader from '../img/awv_logo_header.png';
+import bgsearch from '../img/bg_search.jpg';
+import { clear } from "../reducers/resultsReducer";
 
 class Header extends React.Component {
   render() {
     const {
-      welcome 
+			welcome,
+			clear
     } = this.props;
     return (
-			<div className="App-header">
-				<Link
-          to={'/'}
-        >
-          <img
-            className="App-logo"
-            src={ logoHeader }
-          />
-        </Link>
-				<nav>
+			<div>
+				<div className="App-header">
+					<Link
+						to={'/'}
+					>
+						<img
+							className="App-logo"
+							src={ logoHeader }
+							onClick={
+								() =>{
+									console.log("clear");
+									clear();
+								}
+							}
+						/>
+					</Link>
+					<nav>
+						<Link
+							to={'/about'}
+						>
+							¿Quiénes somos?
+						</Link>
+						<Link
+							to={'/contact'}					
+						>
+							Contacto
+						</Link>
+						<Link
+							to={'/login'}					
+						>
+							Iniciar sesión
+						</Link>
+					</nav>
+				</div>
+				<div
+					className="App-subheader"
+				>
+					<div
+						className="App-subheader-message"
+					>
+							<h1>Busca en nuestro archivo</h1>
+							<p>
+								En nuestro archivo hay almacenadas múltiples páginas de origen venezolano, siéntete libre de revisar.
+							</p>
+					</div>
 					<SearchBar />
-					<Link
-						to={'/about'}
-					>
-						¿Quiénes somos?
-					</Link>
-					<Link
-						to={'/contact'}					
-					>
-						Contacto
-					</Link>
-					<Link
-						to={'/login'}					
-					>
-						Iniciar sesión
-					</Link>
-				</nav>
+				</div>
 			</div>
     );
   }
@@ -50,6 +73,8 @@ const mS = state => {
   };
 };
 
-const mD = {};
+const mD = {
+	clear
+};
 
 export default connect(mS, mD)(Header);
